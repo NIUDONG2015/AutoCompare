09bf7aa1a1bd362cb98e0cfb8b98a5c2221c92eb
salute <851338671@qq.com>
2016-06-05 12:59:42 +0800

    update PSection.java

--- src/cn/edu/zufe/drawable/PSection.java
+++ src/cn/edu/zufe/drawable/PSection.java
@@ -1,5 +1,20 @@
 package cn.edu.zufe.drawable;
 
-public class PSection {
+import processing.core.*;
+import cn.edu.zufe.model.*;
 
+public class PSection {
+	private Well well;
+	
+	public PSection(Well well) {
+		this.well = well;
+	}
+	
+	public void draw() {
+		
+	}
+	
+	public void connect(PSection ps) {
+		
+	}
 }
