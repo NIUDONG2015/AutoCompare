93cc2b38bd4b1f261461067078afb3ab875343ea
huiyadanli <huiyadanli@gmail.com>
2016-06-25 13:07:35 +0800

    界面事件完善

--- src/cn/edu/zufe/ui/FrameMain.java
+++ src/cn/edu/zufe/ui/FrameMain.java
@@ -1,7 +1,5 @@
 package cn.edu.zufe.ui;
 
-import java.awt.Color;
-import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridBagConstraints;
@@ -9,6 +7,10 @@ import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.awt.event.TextEvent;
+import java.awt.event.TextListener;
 import java.io.File;
 import java.io.IOException;
 import java.util.LinkedList;
@@ -23,21 +25,22 @@ import cn.edu.zufe.match.MatchFactory;
 import cn.edu.zufe.model.*;
 import cn.edu.zufe.drawable.*;
 
-public class FrameMain extends JFrame implements ActionListener, ChangeListener {
+public class FrameMain extends JFrame implements ActionListener, ChangeListener, TextListener, FocusListener {
 
 	private JMenuBar menubar;
 	private JMenu menuFile, menuTest, menuSort, menuAutoSorting;
 	private JMenuItem miOpen, miSave, miExport, miExit, miSaveAsImage, miManualSorting, miSort1, miSort2; // 菜单项
 
-	private JLabel labelSCWellWidth;
-	private JSlider sliderSCWellWidth;
-	private JTextField txtSCWellWidth;
+	private JLabel labelSCWellWidth, labelPixelRatio;
+	private JSlider sliderSCWellWidth, sliderPixelRatio;
+	private JTextField txtSCWellWidth, txtPixelRatio;
 
 	private PAppletWellView pwv; // 油井视图
 	private PAppletSC psc; // 地层对比图
 	private LinkedList<Well> wellList;
 	private MatchFactory matchFactory;
 
+	private final static int MENU_WIDTH = 40, MENU_HEIGHT = 20;
 	private final static int PWV_WIDTH = 460, PWV_HEIGHT = 410;
 	private final static int PSC_WIDTH = 500, PSC_HEIGHT = 600;
 
@@ -68,6 +71,8 @@ public class FrameMain extends JFrame implements ActionListener, ChangeListener 
 		menubar = new JMenuBar();
 		// 文件菜单项
 		menuFile = new JMenu("File");
+		menuFile.setPreferredSize(new Dimension(MENU_WIDTH, MENU_HEIGHT));
+		// menuFile.setHorizontalTextPosition(JMenu.CENTER);
 		miOpen = new JMenuItem("Open");
 		miSave = new JMenuItem("Save");
 		miExport = new JMenuItem("Export");
@@ -83,6 +88,7 @@ public class FrameMain extends JFrame implements ActionListener, ChangeListener 
 
 		// 手动排序和自动排序
 		menuSort = new JMenu("Sort");
+		menuSort.setPreferredSize(new Dimension(MENU_WIDTH, MENU_HEIGHT));
 		miManualSorting = new JMenuItem("ManualSorting");
 		menuAutoSorting = new JMenu("AutoSorting");
 		miSort1 = new JMenuItem("sort1");
@@ -97,7 +103,8 @@ public class FrameMain extends JFrame implements ActionListener, ChangeListener 
 		miSort2.addActionListener(this);
 
 		// 测试工具菜单项
-		menuTest = new JMenu("Test Tool");
+		menuTest = new JMenu("Test");
+		menuTest.setPreferredSize(new Dimension(MENU_WIDTH, MENU_HEIGHT));
 		miSaveAsImage = new JMenuItem("Save As Image");
 		menuTest.add(miSaveAsImage);
 		menubar.add(menuTest);
@@ -110,7 +117,7 @@ public class FrameMain extends JFrame implements ActionListener, ChangeListener 
 		// paramPanel 使用 GridBagLayout 布局
 		GridBagLayout gb = new GridBagLayout();
 		JPanel paramPanel = new JPanel(gb);
-		paramPanel.setPreferredSize(new Dimension(PWV_WIDTH, 300));
+		paramPanel.setPreferredSize(new Dimension(PWV_WIDTH, 100));
 		// 用来控制添加进的组件的显示位置
 		GridBagConstraints gbc = new GridBagConstraints();
 		gbc.fill = GridBagConstraints.HORIZONTAL;
@@ -131,10 +138,35 @@ public class FrameMain extends JFrame implements ActionListener, ChangeListener 
 		paramPanel.add(sliderSCWellWidth);
 
 		txtSCWellWidth = new JTextField(String.valueOf(sliderSCWellWidth.getValue()), 10);
-		gbc.gridwidth = 1;
+		txtSCWellWidth.setHorizontalAlignment(JTextField.CENTER);
+		txtSCWellWidth.addFocusListener(this);
+		gbc.gridwidth = 0;
 		gbc.weightx = 0;
 		gb.setConstraints(txtSCWellWidth, gbc);
 		paramPanel.add(txtSCWellWidth);
+
+		// m/px 像素比
+		labelPixelRatio = new JLabel("Pixel Ratio");
+		gbc.gridwidth = 1;
+		gbc.weightx = 0;
+		gbc.insets = new Insets(0, 0, 10, 10);
+		gb.setConstraints(labelPixelRatio, gbc);
+		paramPanel.add(labelPixelRatio);
+
+		sliderPixelRatio = new JSlider(JSlider.HORIZONTAL, 1, 200, 100);
+		sliderPixelRatio.addChangeListener(this);
+		gbc.gridwidth = 1;
+		gbc.weightx = 1;
+		gb.setConstraints(sliderPixelRatio, gbc);
+		paramPanel.add(sliderPixelRatio);
+
+		txtPixelRatio = new JTextField(String.valueOf(sliderPixelRatio.getValue() * 0.01), 10);
+		txtPixelRatio.setHorizontalAlignment(JTextField.CENTER);
+		txtPixelRatio.addFocusListener(this);
+		gbc.gridwidth = 0;
+		gbc.weightx = 0;
+		gb.setConstraints(txtPixelRatio, gbc);
+		paramPanel.add(txtPixelRatio);
 
 		this.add(paramPanel);
 	}
@@ -147,7 +179,7 @@ public class FrameMain extends JFrame implements ActionListener, ChangeListener 
 			e.printStackTrace();
 		}
 		// 实例化主窗体
-		FrameMain fMain = new FrameMain("AutoCompare", 475, 775);
+		FrameMain fMain = new FrameMain("AutoCompare", 475, 575);
 
 		// 刷新界面风格
 		SwingUtilities.updateComponentTreeUI(fMain);
@@ -214,9 +246,52 @@ public class FrameMain extends JFrame implements ActionListener, ChangeListener 
 
 	@Override
 	public void stateChanged(ChangeEvent e) {
-		// TODO 自动生成的方法存根
 		if (e.getSource() == sliderSCWellWidth) {
 			txtSCWellWidth.setText(String.valueOf(sliderSCWellWidth.getValue()));
+		} else if (e.getSource() == sliderPixelRatio) {
+			txtPixelRatio.setText(String.format("%.2f", sliderPixelRatio.getValue() * 0.01));
+		}
+	}
+
+	@Override
+	public void textValueChanged(TextEvent e) {
+		if (e.getSource() == txtSCWellWidth) {
+			sliderSCWellWidth.setValue(Integer.parseInt(txtSCWellWidth.getText()));
+			System.out.println(txtSCWellWidth.getText());
+		} else if (e.getSource() == txtPixelRatio) {
+			sliderPixelRatio.setValue(Integer.parseInt(txtPixelRatio.getText()) * 100);
+		}
+	}
+
+	@Override
+	public void focusGained(FocusEvent arg0) {
+		// TODO 自动生成的方法存根
+
+	}
+
+	@Override
+	public void focusLost(FocusEvent e) {
+		if (e.getSource() == txtSCWellWidth) {
+			int t = Integer.parseInt(txtSCWellWidth.getText());
+			// 限制范围
+			if (t > sliderSCWellWidth.getMaximum()) {
+				t = sliderSCWellWidth.getMaximum();
+			}
+			if (t < sliderSCWellWidth.getMinimum()) {
+				t = sliderSCWellWidth.getMinimum();
+			}
+			sliderSCWellWidth.setValue(t);
+		} else if (e.getSource() == txtPixelRatio) {
+			int t = (int)(Double.parseDouble(txtPixelRatio.getText()) * 100);
+			// 限制范围
+			if (t > sliderPixelRatio.getMaximum()) {
+				t = sliderPixelRatio.getMaximum();
+			}
+			if (t < sliderPixelRatio.getMinimum()) {
+				t = sliderPixelRatio.getMinimum();
+			}
+			System.out.println(t);
+			sliderPixelRatio.setValue(t);
 		}
 	}
 
